f2e0e871644ca70959868db7c32d05cdf741bd69
anmolsingh1894 <anmol180994@gmail.com>
2022-02-18 00:31:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Anmol_Score
+ Lab4
---
+++ app/src/main/java/com/anmol_a00246505/anmol_score/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anmol_a00246505.anmol_score;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
