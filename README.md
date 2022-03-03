a102a36de0db1fb613da6615a2c2a524eab0b037
anmolsingh1894 <anmol180994@gmail.com>
2022-03-03 02:27:25 -0500

    made the widgets functional
    the buttons, radio buttons and switch are now functional and are used in coordination to change the sore for selected team

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Anmol_Score
- This project is created to complete Lab4 and demonstrate use of layouts and widgets.
+ This project is updated to complete Lab5 and make use of the widgets created peviously.
+ Layout and design are also changed to make the app look better
+ 
+##Description
+ *This app is used to keep score of a game between team Pluto and team Jupiter.
+ *The switch can be used to select the team for which the score needs to be changed.
+ *The Radio Buttons are used to select a number by which the score will change.
+ *The INCREASE/DECREASE Buttons are used to increase/decrease the score.
--- app/src/main/java/com/anmol_a00246505/anmol_score/MainActivity.java
+++ app/src/main/java/com/anmol_a00246505/anmol_score/MainActivity.java
@@ -3,12 +3,112 @@ package com.anmol_a00246505.anmol_score;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView plutoScoreText, jupiterScoreText;
+    Switch teamSelector;
+    RadioGroup scores;
+    Button increaseButton, decreaseButton;
+
+    int teamPluto = 0, teamJupiter = 1;
+    int selectedTeam = teamPluto;
+    int score = 1;
+    int totalScorePluto = 10, totalScoreJupiter = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //==============
+        //assigning objects to widgets used in xml by their id
+        //==============
+        plutoScoreText = findViewById(R.id.plutoScoreText);
+        jupiterScoreText = findViewById(R.id.jupiterScoreText);
+        teamSelector = findViewById(R.id.teamSelector);
+        scores = findViewById(R.id.scores);
+        increaseButton = findViewById(R.id.increaseScoreButton);
+        decreaseButton = findViewById(R.id.decreaseScoreButton);
+
+        //setting current score of the game
+        plutoScoreText.setText(String.valueOf(totalScorePluto));
+        jupiterScoreText.setText(String.valueOf(totalScoreJupiter));
+
+        //==============
+        //adding listeners to objects
+        //==============
+
+        //switch listener checks for change in switch
+        teamSelector.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    selectedTeam = teamJupiter;
+                } else {
+                    selectedTeam = teamPluto;
+                }
+            }
+        });
+
+        //radio group listener checks for change in radio button selection
+        scores.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.score1:
+                        score = 1;
+                        break;
+                    case R.id.score2:
+                        score = 2;
+                        break;
+                    case R.id.score3:
+                        score = 3;
+                        break;
+                    case R.id.score4:
+                        score = 4;
+                        break;
+                    default:
+                        score = 0;
+                        break;
+                }
+            }
+        });
+
+        //button listeners checks if button was clicked
+
+        increaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedTeam == teamPluto){
+                    totalScorePluto += score;
+                    plutoScoreText.setText(String.valueOf(totalScorePluto));
+                }
+                else if(selectedTeam == teamJupiter){
+                    totalScoreJupiter += score;
+                    jupiterScoreText.setText(String.valueOf(totalScoreJupiter));
+                }
+            }
+        });
+
+        decreaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedTeam == teamPluto){
+                    totalScorePluto -= score;
+                    plutoScoreText.setText(String.valueOf(totalScorePluto));
+                }
+                else if(selectedTeam == teamJupiter){
+                    totalScoreJupiter -= score;
+                    jupiterScoreText.setText(String.valueOf(totalScoreJupiter));
+                }
+            }
+        });
+
     }
 }
